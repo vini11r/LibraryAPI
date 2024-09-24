c90e729519cf061c39176615ff23aa4a0dfd254c
Pavel Gornostaev <pavelgornostaeff@yandex.ru>
2024-09-24 20:14:33 +0300

    переопределил retrieve

--- books/serializers.py
+++ books/serializers.py
@@ -8,11 +8,25 @@ class AuthorSerializer(serializers.ModelSerializer):
         model = Author
         fields = ("pk", "first_name", "last_name",)
 
+
+
+
 class BooksSerializer(serializers.ModelSerializer):
-    book_author = AuthorSerializer(source="author", many=True)
     class Meta:
         model = Books
         fields = "__all__"
+
+class AuthorDetailsSerializer(serializers.ModelSerializer):
+    author_book = BooksSerializer(many=True)
+    class Meta:
+        model = Author
+        fields = ("pk", "first_name", "last_name", "author_book")
+
+class BookDetailsSerializer(serializers.ModelSerializer):
+    book_author = AuthorSerializer(source="author", many=True, read_only=True)
+    class Meta:
+        model = Books
+        fields = ("pk", "title", "book_author", "publication_year", "genre", "description")
 
 class BookIssuanceSerializer(serializers.ModelSerializer):
     class Meta:
--- books/views.py
+++ books/views.py
@@ -1,17 +1,31 @@
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 
 from books.models import Books, Author, BookIssuance
-from books.serializers import BooksSerializer, AuthorSerializer, BookIssuanceSerializer
+from books.serializers import BooksSerializer, AuthorSerializer, BookIssuanceSerializer, BookDetailsSerializer, AuthorDetailsSerializer
 
 
 class BooksViewSet(ModelViewSet):
     queryset = Books.objects.all()
     serializer_class = BooksSerializer
 
+    def retrieve(self, request, pk=None, **kwargs):
+        queryset = Books.objects.all().filter(pk=pk)
+        serializer = BookDetailsSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+
 class AuthorViewSet(ModelViewSet):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
+
+    def retrieve(self, request, pk=None, **kwargs):
+        queryset = Author.objects.all().filter(pk=pk)
+        serializer = AuthorDetailsSerializer(queryset, many=True)
+        return Response(serializer.data)
+
 
 class BookIssuanceAPIView(APIView):
     queryset = BookIssuance.objects.all()
