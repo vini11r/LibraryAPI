529dc3f28858f5f3536a06369bbada382a431ab9
Pavel Gornostaev <pavelgornostaeff@yandex.ru>
2024-09-25 21:03:02 +0300

    CRUD для user

--- config/urls.py
+++ config/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("books.urls", namespace="books")),
+    path("", include("users.urls", namespace="users")),
 ]
