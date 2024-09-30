9d3b01836ce84df75378aad6b039f6d3c68724b5
Pavel Gornostaev <pavelgornostaeff@yandex.ru>
2024-09-30 23:37:53 +0300

    Настроил вывод документации

--- config/settings.py
+++ config/settings.py
@@ -23,6 +23,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "rest_framework_simplejwt",
+    "drf_yasg",
     "django_filters",
     "users",
     "books",
--- users/views.py
+++ users/views.py
@@ -2,15 +2,18 @@ from rest_framework.permissions import AllowAny
 from rest_framework.viewsets import ModelViewSet
 
 from users.models import User
-from users.permissions import IsUser, IsLibrarian
-from users.serializers import (UserCreateUpdateSerializer,
-                               UserDetailsSerializer, UserSerializer)
+from users.permissions import IsLibrarian, IsUser
+from users.serializers import (
+    UserCreateUpdateSerializer,
+    UserDetailsSerializer,
+    UserSerializer,
+)
 
 
 class UserViewSet(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = (IsUser, )
+    permission_classes = (IsUser,)
 
     def get_serializer_class(self):
         if self.action == "retrieve":
@@ -23,12 +26,10 @@ class UserViewSet(ModelViewSet):
         if self.action == "create":
             self.permission_classes = (AllowAny,)
         elif self.action == "list":
-            self.permission_classes = (IsLibrarian, )
+            self.permission_classes = (IsLibrarian,)
         elif self.action in ["retrieve", "update", "destroy", "partial_update"]:
-            self.permission_classes = (IsLibrarian | IsUser, )
+            self.permission_classes = (IsLibrarian | IsUser,)
         return super().get_permissions()
-
-
 
     def perform_create(self, serializer):
         user = serializer.save()
